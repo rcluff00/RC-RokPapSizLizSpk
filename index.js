7a443056bc1cf2a7b8773dd885cd3c9f1ed3087d
rcluff00 <31253741+rcluff00@users.noreply.github.com>
2022-09-21 15:20:06 -0600

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-// run `node index.js` in the terminal
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
